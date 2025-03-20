71883a689c8cbf810166877e606f3bc5f44c4ab9
lvzrr <lvzrr@proton.me>
2025-03-20 11:26:43 +0100

    read impl

--- problema 1/problema-1.py
+++ problema 1/problema-1.py
@@ -3,6 +3,7 @@ def read(n_calles):
     m = [[0] * n_calles for i in range(n_inters)]
     for i in range(n_calles):
         line = input().split(" ")
+        print(line)
         m[int(line[0]) - 1][int(line[1]) - 1] += 1
         m[int(line[1]) - 1][int(line[0]) - 1] += 1
     return m
