a1484c80af7170b42524f64fec2b0e1d79d53e2f
lvzrr <lvzrr@proton.me>
2025-03-20 11:07:03 +0100

    read impl

--- read.py
+++ read.py
@@ -1,12 +1,11 @@
 def read():
     n_calles = int(input())
     n_inters = int(input())
-    m = [[0] * n_calles] * n_calles
+    m = [[0] * n_calles for i in range(n_calles)]
     for i in range(n_calles):
         line = input().split(" ")
         m[int(line[0]) - 1][int(line[1]) - 1] += 1
         m[int(line[1]) - 1][int(line[0]) - 1] += 1
         print(m)
     return m
-
 print(read())
