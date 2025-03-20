4b332d76ae04e764e0278faa5a365edc141ff786
Lucas Winand <lucas.winand@hotmail.es>
2025-03-20 11:22:52 +0100

    Folder structure

--- read.py
+++
@@ -1,9 +0,0 @@
-def read():
-    n_calles = int(input())
-    n_inters = int(input())
-    m = [[0] * n_inters for i in range(n_inters)]
-    for i in range(n_calles):
-        line = input().split(" ")
-        m[int(line[0]) - 1][int(line[1]) - 1] += 1
-        m[int(line[1]) - 1][int(line[0]) - 1] += 1
-    return m
