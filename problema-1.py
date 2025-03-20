43b7a61a04c208efeffd2c6bbed551395c3add4a
Lucas Winand <lucas.winand@hotmail.es>
2025-03-20 11:22:08 +0100

    problema 1 solved

--- problema-1.py
+++ problema-1.py
@@ -1,17 +1,33 @@
-import read
+def read(n_calles):
+    n_inters = int(input())
+    m = [[0] * n_calles for i in range(n_inters)]
+    for i in range(n_calles):
+        line = input().split(" ")
+        m[int(line[0]) - 1][int(line[1]) - 1] += 1
+        m[int(line[1]) - 1][int(line[0]) - 1] += 1
+    return m
 
-matriz_grafo = read.read();
-impares = 0
+def es_recorrido_euclidiano(matriz_grafo):
+	impares = 0
 
-for i in range(len(matriz_grafo)):
-	grado = 0
-	for j in range(len(matriz_grafo[i])):
-		grado += matriz_grafo[i][j]
+	for i in range(len(matriz_grafo)):
+		grado = 0
+		for j in range(len(matriz_grafo[i])):
+			grado += matriz_grafo[i][j]
 	
 	if grado % 2 != 0:
 		impares += 1
 
-if impares == 0 or impares == 2:
-	print("SI")
-else:
-	print("NO")
+	return impares == 0 or impares == 2
+
+if __name__ == "__main__":
+		while True:
+			n_calles = int(input())
+
+			if n_calles == 0:
+				break
+
+			if es_recorrido_euclidiano(read(n_calles)):
+					print("SI")
+			else:
+					print("NO")
