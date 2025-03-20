f528faeafbee50f2697822a35045740a58e3fb3e
GIOVANNI-GAMER <giovannisupercrack@gmail.com>
2025-03-20 11:12:15 +0100

    inters

--- read.py
+++ read.py
@@ -1,7 +1,7 @@
 def read():
     n_calles = int(input())
     n_inters = int(input())
-    m = [[0] * n_calles for i in range(n_calles)]
+    m = [[0] * n_calles for i in range(n_inters)]
     for i in range(n_calles):
         line = input().split(" ")
         m[int(line[0]) - 1][int(line[1]) - 1] += 1
